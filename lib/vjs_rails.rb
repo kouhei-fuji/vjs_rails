b66af3c9f19d65e6dcdaec5d2731086814a24f2d
Kouhei Fujigaya <kouhei.fujigaya@gmail.com>
2014-10-27 12:26:40 +0900

    add Rails Railtie & Engine

--- lib/vjs_rails.rb
+++ lib/vjs_rails.rb
@@ -1,2 +1,4 @@
-module VjsRails
-end
+# encoding: utf-8
+require 'vjs_rails/railtie'
+require 'vjs_rails/engine'
+require 'vjs_rails/version'
---
+++ lib/vjs_rails/engine.rb
@@ -0,0 +1,6 @@
+# encoding: utf-8
+
+module VjsRails
+  class Engine < ::Rails::Engine
+  end
+end
